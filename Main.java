a26eae2af2eddf2b9914e55bd9884596f43cdf55
jarek167714 <101186879+jarek167714@users.noreply.github.com>
2022-03-22 11:39:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) 
+  {
+    int a = 3;
+    double b = 21;
+    char c = 'j';
+    String d = "Jarosław";
+
+    System.out.println("a = " +a+ "\nb = " + b + "\nc = " + c +"\nd = "+d);
+    
+  }
+}
